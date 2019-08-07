961393d5033cee4fc0efe34d485f7c8e4d805d37
ftnext <takuyafjp+develop@gmail.com>
2019-08-07 22:10:29 +0900

    [def] implemented Py4kaImage.write method
    * drawはプロパティにする必要はなかったかもしれない

--- python4kyoani/image.py
+++ python4kyoani/image.py
@@ -1,9 +1,19 @@
-from PIL import Image
+from PIL import Image, ImageDraw
 
 
 class Py4kaImage:
     def __init__(self, image_path):
         self.image = Image.open(image_path)
+        self.draw = None
+
+    def _start_coordinate_of_text(self, message):
+        raise NotImplementedError
+
+    def write(self, message):
+        if self.draw is None:
+            self.draw = ImageDraw.Draw(self.image)
+        start_coordinate = self._start_coordinate_of_text(message)
+        self.draw.text(start_coordinate, message, fill='gray')
 
 
 def create_py4kaimage(image_path):
--- tests/test_image.py
+++ tests/test_image.py
@@ -1,5 +1,5 @@
 from unittest import TestCase
-from unittest.mock import MagicMock, patch
+from unittest.mock import call, MagicMock, patch
 
 import python4kyoani.image as i
 
@@ -13,3 +13,44 @@ class TestCreatePy4kaimage(TestCase):
 
         self.assertIsInstance(img, i.Py4kaImage)
         self.assertEqual(img.image, image_open.return_value)
+        self.assertIsNone(img.draw)
+
+
+class TestPy4kaImageWrite(TestCase):
+    @patch('python4kyoani.image.Image.open')
+    @patch('python4kyoani.image.ImageDraw.Draw')
+    @patch('python4kyoani.image.Py4kaImage._start_coordinate_of_text')
+    def test_draw_not_set(
+            self, start_coordinate_of_text, image_draw_draw, mock_open):
+        draw = image_draw_draw.return_value
+        start_coordinate = start_coordinate_of_text.return_value
+
+        image_path = MagicMock()
+        img = i.Py4kaImage(image_path)
+        img.draw = None
+        message = MagicMock()
+        img.write(message)
+
+        self.assertEqual([call(img.image)], image_draw_draw.call_args_list)
+        self.assertEqual(
+            [call(message)], start_coordinate_of_text.call_args_list)
+        self.assertEqual(
+            [call(start_coordinate, message, fill='gray')],
+            draw.text.call_args_list)
+        self.assertEqual(draw, img.draw)
+
+    @patch('python4kyoani.image.Image.open')
+    @patch('python4kyoani.image.Py4kaImage._start_coordinate_of_text')
+    def test_draw_set(self, start_coordinate_of_text, mock_open):
+        start_coordinate = start_coordinate_of_text.return_value
+
+        image_path = MagicMock()
+        img = i.Py4kaImage(image_path)
+        message = MagicMock()
+        img.write(message)
+
+        self.assertEqual(
+            [call(message)], start_coordinate_of_text.call_args_list)
+        self.assertEqual(
+            [call(start_coordinate, message, fill='gray')],
+            img.draw.text.call_args_list)
